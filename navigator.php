add59e926f1d5adc75beb050d72694bc0de849b6
Korawich-sd <76939243+Korawich-sd@users.noreply.github.com>
2022-11-26 15:26:23 +0700

    finish blog system 26/11/65

--- navigator.php
+++ navigator.php
@@ -1,7 +1,15 @@
+<?php
+require_once('config/bddesign_db.php');
+$page = $_GET['page'];
+
+if($page == ""){
+	$page = 1;
+}
+?>
 <div class="bg-navigator">
 	<div class="container-xxl">
 		<a href="index.php">Home</a>
-		<a href="#">Page</a>
+		<a href="#">Page <?= $page ?></a>
 	</div>
 </div>
 <div class="clearfix"></div>
